e7995a30ca5334c331db9e77cba0f1e761c4417f
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-07-31 22:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+// function volume_sphere() {
+//     //Write your code here
+// 	// let volume;
+//   let radius = parseFloat(document.getElementById('radius').value);
+//   // radius = Math.abs(radius);
+//   let volume = (4/3) * 3.14 * (radius * radius * radius);
+  
+//   document.getElementById('volume').value = volume;
+//   return false;
+  
+// } 
+
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
 function volume_sphere() {
-    //Write your code here
-	// let volume;
   let radius = parseFloat(document.getElementById('radius').value);
-  // radius = Math.abs(radius);
-  let volume = (4/3) * 3.14 * (radius * radius * radius);
+  let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  volume = volume.toFixed(4); // Round to 4 decimal places
   
   document.getElementById('volume').value = volume;
   return false;
-  
-} 
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+  document.getElementById('MyForm').onsubmit = volume_sphere;
+};
